375b82ac7ce57308547e52ac74efeacf55cc06e4
Max <max@xal.no>
2023-07-06 23:33:32 +0200

    day3, part 1

--- src/advent/Models/day2.cs
+++ src/advent/Models/day2.cs
@@ -36,3 +36,191 @@ static class ReadFile
         return linesWithChars;
     }
 }
+
+class Day2
+{
+    public static void runDay2Part1()
+    {   
+        int score = 0;
+        string FilePath = "src/advent/Models/input2.txt";
+        var listChars = ReadFile.ReadLinesWithChars(FilePath);
+        foreach ((char,char) line in listChars)
+        {
+            // We use the unicode converter
+            int opponent = System.Convert.ToInt32(line.Item1);
+            int mine = System.Convert.ToInt32(line.Item2);
+
+            // Rock
+            if (mine == 88)
+            {
+                score += 1;
+                // Draw
+                if (mine == opponent + 23)
+                {
+                    score += 3;
+                }
+                // Win
+                else if (mine == opponent + 21)
+                {
+                    score += 6;
+                }
+                // Loss
+                else if (mine == opponent + 22)
+                {
+                    score += 0;
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                }
+
+            }
+            // Paper
+            else if (mine == 89)
+            {
+                score += 2;
+                // Draw
+                if (mine == opponent + 23)
+                {
+                    score += 3;
+                }
+                // Win
+                else if (mine == opponent + 24)
+                {
+                    score += 6;
+                }
+                // Loss
+                else if (mine == opponent + 22)
+                {
+                    score += 0;
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                }
+            }
+            // Scissor
+            else if (mine == 90)
+            {
+                score += 3;
+                // Draw
+                if (mine == opponent + 23)
+                {
+                    score += 3;
+                }
+                // Win
+                else if (mine == opponent + 24)
+                {
+                    score += 6;
+                }
+                // Loss
+                else if (mine == opponent + 25)
+                {
+                    score += 0;
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong dur");
+            }
+        }
+        Console.WriteLine($"Final score: {score}");
+    }
+    public static void runDay2Part2()
+    {   
+        int score = 0;
+        string FilePath = "src/advent/Models/input2.txt";
+        var listChars = ReadFile.ReadLinesWithChars(FilePath);
+        foreach ((char,char) line in listChars)
+        {
+            // We use the unicode converter
+            int opponent = System.Convert.ToInt32(line.Item1);
+            int mine = System.Convert.ToInt32(line.Item2);
+
+            // Loose
+            if (mine == 88)
+            {
+                score += 0;
+                // Rock
+                if (opponent == 65)
+                {
+                    score += 3;
+                }
+                // Paper
+                else if (opponent == 66)
+                {
+                    score += 1;
+                }
+                // Scissor
+                else if (opponent == 67)
+                {
+                    score += 2;
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                }
+
+            }
+            // Draw
+            else if (mine == 89)
+            {
+                score += 3;
+                // Rock
+                if (opponent == 65)
+                {
+                    score += 1;
+                }
+                // Paper
+                else if (opponent == 66)
+                {
+                    score += 2;
+                }
+                // Scissor
+                else if (opponent == 67)
+                {
+                    score += 3;
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                }
+
+            }
+            // Win
+            else if (mine == 90)
+            {
+                score += 6;
+                // Rock
+                if (opponent == 65)
+                {
+                    score += 2;
+                }
+                // Paper
+                else if (opponent == 66)
+                {
+                    score += 3;
+                }
+                // Scissor
+                else if (opponent == 67)
+                {
+                    score += 1;
+                }
+                else
+                {
+                    Console.WriteLine("WRONG!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong dur");
+            }
+        }
+        Console.WriteLine($"Final score part 2: {score}");
+    }
+}
---
+++ src/advent/Models/day3.cs
@@ -0,0 +1,103 @@
+using System;
+
+
+static class FileReader
+{
+    public static List<string> ReadLinesFile(string FilePath)
+    {
+        List<string> lines = new List<string>();
+        lines = File.ReadAllLines(FilePath).ToList();
+
+        return lines;
+    }
+}
+
+static class StringMethods
+{
+    public static List<(string,string)> SplitInTwo(List<string> lines)
+    {
+        List<(string,string)> to_return = new List<(string,string)>();
+
+        foreach (string line in lines)
+        {
+            int halfLength = line.Length / 2;
+            string first_half = line.Substring(0, halfLength);
+            string second_half = line.Substring(halfLength);
+            // Sanity check
+            if (first_half.Length != second_half.Length)
+            {
+                throw new Exception("Lengths should be equal");
+            }
+            else
+            {
+                to_return.Add((first_half,second_half));
+            }
+        }
+        return to_return;
+    }
+    // Method to find the common between both strings
+    public static List<string> FindCommon(List<(string,string)> lines)
+    {
+        List<string> commonChars = new List<string>();
+        foreach ((string,string) l in lines)
+        {
+            foreach (char c1 in l.Item1)
+            {
+                if (l.Item2.Contains(c1))
+                {
+                    commonChars.Add(c1.ToString());
+                    break; // We need to break the loop because there might be more matches, but we only care about one!
+                 }
+            }
+        }
+        return commonChars;
+    }
+    // Method to get the priorities
+    public static int CalculatePriorities(List<string> commons)
+    {
+        int total = 0;
+        foreach (string c in commons)
+        {
+            int p = System.Convert.ToInt32(c[0]);
+            int to_add = 0;
+            // If lower case, it is larger than 90
+            if (p > 90)
+            {
+                to_add = p-96;
+                total += to_add;
+            }
+            else if (p < 95)
+            {
+                to_add = p-38;
+                total += to_add;
+            }
+            else
+            {
+                throw new Exception("CHAOS");
+            }
+        }
+        return total;
+    }
+}
+
+class Day3
+{
+    public static void Part1()
+    {
+        string filePath = "src/advent/Models/input3.txt"; 
+
+        // Read the file
+        List<string> linesFile = FileReader.ReadLinesFile(filePath);
+
+        // Split the strings in two parts
+        List<(string,string)> spliitedLines = StringMethods.SplitInTwo(linesFile);
+
+        // Find the common character
+        List<string> commonChars = StringMethods.FindCommon(spliitedLines);
+
+        // Calculate priorities
+        int total = StringMethods.CalculatePriorities(commonChars);
+
+        Console.WriteLine($"Sum of priorities: {total}");
+    }
+}
