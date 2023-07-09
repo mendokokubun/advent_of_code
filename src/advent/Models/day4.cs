953d24199a333e0e2ff1a0f58003a7ea0ec68c85
Max <max@xal.no>
2023-07-09 12:57:21 +0200

    part 1 of day 4

---
+++ src/advent/Models/day4.cs
@@ -0,0 +1,52 @@
+using System;
+
+// Read it pairwise
+
+class Day4
+{
+
+    public static void Part1()
+    {   
+        // Read the file linewise
+        string filePath = "src/advent/Models/input4.txt";
+        
+        using  (StreamReader reader = new StreamReader(filePath))
+        {
+            // Number of containing
+            int numbers = 0;
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                // Split 
+                string[] sacks = line.Split(",");
+                // Get the range of numbers
+                List<int> line1 = ConvertRangeToList(sacks[0]);
+                List<int> line2 = ConvertRangeToList(sacks[1]);
+                // Compare if all elements of one are contained in the other
+                bool isContained1 = line1.All(line2.Contains);
+                bool isContained2 = line2.All(line1.Contains);
+                Console.WriteLine($"First: {isContained1}. Second: {isContained2}");
+                Console.WriteLine("-----------------------");
+                if (isContained1 || isContained2)
+                {
+                    numbers = numbers + 1;
+                }
+            }
+            Console.WriteLine(numbers);
+        }
+    }
+    public static List<int> ConvertRangeToList(string input)
+    {
+        // First get the limiting intervals
+        string[] parts = input.Split("-");
+        int startNumber = int.Parse(parts[0]);
+        int endNumber = int.Parse(parts[1]);
+        // Generate the list
+        List<int> list_integers = new List<int>();
+        for (int i = startNumber; i <= endNumber; i++)
+        {
+            list_integers.Add(i);
+        }
+        return list_integers;
+    }
+}
