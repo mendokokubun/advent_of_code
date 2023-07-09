19b4250ddddf205a7ec442c1a1b6b7555311b81a
Max <max@xal.no>
2023-07-09 13:04:39 +0200

    part 2

--- src/advent/Models/day4.cs
+++ src/advent/Models/day4.cs
@@ -25,11 +25,40 @@ class Day4
                 // Compare if all elements of one are contained in the other
                 bool isContained1 = line1.All(line2.Contains);
                 bool isContained2 = line2.All(line1.Contains);
-                Console.WriteLine($"First: {isContained1}. Second: {isContained2}");
-                Console.WriteLine("-----------------------");
                 if (isContained1 || isContained2)
                 {
                     numbers = numbers + 1;
+                }
+            }
+            Console.WriteLine(numbers);
+        }
+    }
+    public static void Part2()
+    {   
+        // Read the file linewise
+        string filePath = "src/advent/Models/input4.txt";
+        
+        using  (StreamReader reader = new StreamReader(filePath))
+        {
+            // Number of overlaps
+            int numbers = 0;
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                // Split 
+                string[] sacks = line.Split(",");
+                // Get the range of numbers
+                List<int> line1 = ConvertRangeToList(sacks[0]);
+                List<int> line2 = ConvertRangeToList(sacks[1]);
+                // Go through all elements of list 1
+                foreach (int l in line1)
+                {
+                    bool isContainedSingle = line2.Contains(l);
+                    if (isContainedSingle)
+                    {
+                        numbers = numbers + 1;
+                        break;
+                    }
                 }
             }
             Console.WriteLine(numbers);
